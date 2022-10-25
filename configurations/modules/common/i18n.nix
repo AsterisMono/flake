8a9540eff0eb0b3f1fd2610521bfd606c3e0079b
AsterisMono <cmiki@amono.me>
2022-10-25 22:56:30 +0800

    Remove duplicated ime

--- configurations/modules/common/i18n.nix
+++ configurations/modules/common/i18n.nix
@@ -6,12 +6,5 @@
   i18n = {
     defaultLocale = "en_US.UTF-8";
     supportedLocales = [ "zh_CN.UTF-8/UTF-8" "en_US.UTF-8/UTF-8" ];
-    inputMethod = {
-      enabled = "fcitx5";
-      fcitx5.addons = with pkgs; [
-        fcitx5-chinese-addons
-        fcitx5-configtool
-      ];
-    };
   };
 }
