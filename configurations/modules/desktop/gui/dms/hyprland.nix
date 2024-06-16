720ada59f9cf7cc58c097f52bcb6fbf51870000d
Chatnoir Miki <cmiki@amono.me>
2024-06-16 09:41:59 +0800

    chore: Add Hyprland configuration for GUI DMS module

---
+++ configurations/modules/desktop/gui/dms/hyprland.nix
@@ -0,0 +1,33 @@
+{ config, pkgs, ... }:
+
+{
+
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+  };
+
+  environment.systemPackages = with pkgs;[
+    waybar
+    mako
+    wofi
+    libnotify
+    hyprpaper
+    kitty
+  ];
+
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+        user = "greeter";
+      };
+    };
+  };
+
+}
