b0156483fc96de768421ab3c5c904e19f4caf508
Chatnoir Miki <cmiki@amono.me>
2024-06-16 12:09:52 +0800

    Pin home-manager version

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nvim-config = {
