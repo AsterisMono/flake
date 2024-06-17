98a74a3977b358b2b16b3687c1afcee53540e642
Chatnoir Miki <cmiki@amono.me>
2024-06-17 19:18:33 +0800

    User specific LANG

--- configurations/modules/common/i18n.nix
+++ configurations/modules/common/i18n.nix
@@ -4,7 +4,7 @@
   time.timeZone = "Asia/Shanghai";
 
   i18n = {
-    defaultLocale = "zh_CN.UTF-8";
+    defaultLocale = "en_US.UTF-8";
     supportedLocales = [ "zh_CN.UTF-8/UTF-8" "en_US.UTF-8/UTF-8" ];
   };
 }
