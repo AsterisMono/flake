6313846657f87d0b71cb323df4d25e497b23f5ac
Chatnoir Miki <cmiki@amono.me>
2024-06-22 15:39:00 +0800

    feat: purge agenix

--- secrets/secrets.nix
+++
@@ -1,8 +0,0 @@
-# This file is NOT imported into NixOS configuration.
-let
-  cmiki = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIESmYINQDHO1+7FY0mDdcl+UIu2RPuMNOtj242d2N3cf";
-in
-{
-  "tailscale-authkey.age".publicKeys = [ cmiki ];
-  "wakatime-apikey.age".publicKeys = [ cmiki ];
-}  
