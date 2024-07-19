e4f83667bd0c48a083c2c45361dad43cc58784ef
Chatnoir Miki <cmiki@amono.me>
2024-07-19 22:35:44 +0800

    chore: update

--- configurations/users/cmiki/default.nix
+++ configurations/users/cmiki/default.nix
@@ -2,7 +2,7 @@
 {
   users.users.cmiki = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "video" "docker" "networkmanager" "input" ];
+    extraGroups = [ "wheel" "video" "docker" "networkmanager" "input" "docker" ];
     shell = if isDesktop then pkgs.fish else pkgs.bash;
     initialHashedPassword = "$6$a1m7k9D8YB4CnDqv$ZOEZ40sles1ibSoYUv365CWWpplxkHsoDIEkKTZW1pZZBzcUH0Cfjy1G0ssq5rviKaH/Z1MIEnmrgDZPR.V1Y/";
     openssh.authorizedKeys.keys = [
