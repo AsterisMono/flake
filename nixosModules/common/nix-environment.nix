9eb3a2521a48a73b480b649155a3c5ff3bded343
Chatnoir Miki <cmiki@amono.me>
2024-07-26 13:14:32 +0800

    feat: auto gc

--- nixosModules/common/nix-environment.nix
+++ nixosModules/common/nix-environment.nix
@@ -22,5 +22,21 @@
 
   nixpkgs.config.allowUnfree = true;
 
+  # do not need to keep too much generations
+  boot.loader.systemd-boot.configurationLimit = 10;
+
+  # do garbage collection weekly to keep disk usage low
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 1w";
+  };
+
+  # Optimise storage
+  # you can also optimise the store manually via:
+  #    nix-store --optimise
+  # https://nixos.org/manual/nix/stable/command-ref/conf-file.html#conf-auto-optimise-store
+  nix.settings.auto-optimise-store = true;
+
   system.stateVersion = "24.05";
 }
