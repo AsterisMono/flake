0267dbbda6747f46652352e9e29d8b384b72f61c
Chatnoir Miki <cmiki@amono.me>
2024-08-12 16:21:04 +0800

    feat(darwin): include config for iterm2

---
+++ homeModules/darwin/iterm2/default.nix
@@ -0,0 +1,8 @@
+{ ... }:
+{
+  # Need to manually config iterm2 to use this settings file.
+  # Settings - General - Settings - Load settings from a custom folder or URL
+  home.file.".config/iterm2/com.googlecode.iterm2.plist" = {
+    source = ./com.googlecode.iterm2.plist;
+  };
+}
