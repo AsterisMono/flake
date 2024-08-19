5732f481c78a76dae4b30c8d9f2b6076c8eddf14
Chatnoir Miki <cmiki@amono.me>
2024-08-19 14:31:43 +0800

    feat: update server modules

--- nixosModules/users/cmiki.nix
+++ nixosModules/users/cmiki.nix
@@ -4,7 +4,7 @@ with pkgs.lib;
 {
   users.users.cmiki = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "video" "docker" "networkmanager" "input" "docker" ];
+    extraGroups = [ "wheel" "video" "docker" "networkmanager" "input" "wireshark" ];
     shell = pkgs.fish;
     initialHashedPassword = "$6$a1m7k9D8YB4CnDqv$ZOEZ40sles1ibSoYUv365CWWpplxkHsoDIEkKTZW1pZZBzcUH0Cfjy1G0ssq5rviKaH/Z1MIEnmrgDZPR.V1Y/";
     openssh.authorizedKeys.keys = [
