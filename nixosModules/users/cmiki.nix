d4b44daa316843a6469bd16a12c3a21fa9bbaa50
Chatnoir Miki <cmiki@amono.me>
2024-08-19 15:12:30 +0800

    feat: use bash for servers

--- nixosModules/users/cmiki.nix
+++ nixosModules/users/cmiki.nix
@@ -5,7 +5,7 @@ with pkgs.lib;
   users.users.cmiki = {
     isNormalUser = true;
     extraGroups = [ "wheel" "video" "docker" "networkmanager" "input" "wireshark" ];
-    shell = pkgs.fish;
+    shell = mkDefault pkgs.fish;
     initialHashedPassword = "$6$a1m7k9D8YB4CnDqv$ZOEZ40sles1ibSoYUv365CWWpplxkHsoDIEkKTZW1pZZBzcUH0Cfjy1G0ssq5rviKaH/Z1MIEnmrgDZPR.V1Y/";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIESmYINQDHO1+7FY0mDdcl+UIu2RPuMNOtj242d2N3cf"
