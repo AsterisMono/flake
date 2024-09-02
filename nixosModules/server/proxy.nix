27d2776d875ef9b51c965b2a221925575b6dcf00
Chatnoir Miki <cmiki@amono.me>
2024-09-02 09:47:11 +0800

    fix: server proxy option not respected

--- nixosModules/server/proxy.nix
+++ nixosModules/server/proxy.nix
@@ -1,16 +1,19 @@
-{ secrets, ... }:
+{ lib, config, secrets, ... }:
 let
   httpProxy = "http://127.0.0.1:7890";
   socksProxy = "socks5://127.0.0.1:7890";
+  cfg = config.amono.server.proxy;
 in
 {
-  services.mihomo = {
-    enable = true;
-    configFile = "${secrets}/mihomoConfig.yaml";
-  };
-  networking.proxy = {
-    inherit httpProxy;
-    httpsProxy = httpProxy;
-    allProxy = socksProxy;
+  config = lib.mkIf cfg.enable {
+    services.mihomo = {
+      enable = true;
+      configFile = "${secrets}/mihomoConfig.yaml";
+    };
+    networking.proxy = {
+      inherit httpProxy;
+      httpsProxy = httpProxy;
+      allProxy = socksProxy;
+    };
   };
 }
