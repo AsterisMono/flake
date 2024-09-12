74d4923947a3f5d270d24acd5e0307d3053086c0
Chatnoir Miki <cmiki@amono.me>
2024-09-12 19:28:01 +0800

    refactor(nixos): move options to each module

--- nixosModules/common/tailscale.nix
+++ nixosModules/common/tailscale.nix
@@ -1,37 +1,46 @@
 { config, pkgs, secrets, ... }:
 {
-  environment.systemPackages = [ pkgs.tailscale ];
-  services.tailscale.enable = true;
-  networking.firewall = {
-    trustedInterfaces = [ "tailscale0" ];
-    allowedUDPPorts = [ config.services.tailscale.port ];
-    allowedTCPPorts = [ 22 ];
+  options = {
+    amono.tailscale.enable = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Auto register and enable tailscale";
+    };
   };
-  systemd.services.tailscale-autoconnect = {
-    description = "Automatic connection to Tailscale";
+  config = {
+    environment.systemPackages = [ pkgs.tailscale ];
+    services.tailscale.enable = true;
+    networking.firewall = {
+      trustedInterfaces = [ "tailscale0" ];
+      allowedUDPPorts = [ config.services.tailscale.port ];
+      allowedTCPPorts = [ 22 ];
+    };
+    systemd.services.tailscale-autoconnect = {
+      description = "Automatic connection to Tailscale";
 
-    # make sure tailscale is running before trying to connect to tailscale
-    after = [ "network-pre.target" "tailscale.service" ];
-    wants = [ "network-pre.target" "tailscale.service" ];
-    wantedBy = [ "multi-user.target" ];
+      # make sure tailscale is running before trying to connect to tailscale
+      after = [ "network-pre.target" "tailscale.service" ];
+      wants = [ "network-pre.target" "tailscale.service" ];
+      wantedBy = [ "multi-user.target" ];
 
-    # set this service as a oneshot job
-    serviceConfig.Type = "oneshot";
+      # set this service as a oneshot job
+      serviceConfig.Type = "oneshot";
 
-    # have the job run this shell script
-    script = with pkgs; ''
-      secret=${builtins.readFile "${secrets}/tailscaleAuthKey"}
-      # wait for tailscaled to settle
-      sleep 2
+      # have the job run this shell script
+      script = with pkgs; ''
+        secret=${builtins.readFile "${secrets}/tailscaleAuthKey"}
+        # wait for tailscaled to settle
+        sleep 2
 
-      # check if we are already authenticated to tailscale
-      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
-      if [ $status = "Running" ]; then # if so, then do nothing
-        exit 0
-      fi
+        # check if we are already authenticated to tailscale
+        status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
+        if [ $status = "Running" ]; then # if so, then do nothing
+          exit 0
+        fi
 
-      # otherwise authenticate with tailscale
-      ${tailscale}/bin/tailscale up -authkey $secret
-    '';
+        # otherwise authenticate with tailscale
+        ${tailscale}/bin/tailscale up -authkey $secret
+      '';
+    };
   };
 }
