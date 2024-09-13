a4653b0f0f82780593efec7528013635d8657532
Chatnoir Miki <cmiki@amono.me>
2024-09-13 17:07:36 +0800

    wip: nixos docs runner

--- packages/nixos-docs.nix
+++ packages/nixos-docs.nix
@@ -1,4 +1,4 @@
-{ lib, nixosOptionsDoc, modules }:
+{ lib, writeShellScriptBin, nixosOptionsDoc, modules, pkgs }:
 let
   eval = lib.evalModules {
     modules = [
@@ -7,7 +7,14 @@ let
       }
     ] ++ modules;
   };
+  inherit (nixosOptionsDoc {
+    inherit (eval) options;
+  }) optionsJSON;
 in
-(nixosOptionsDoc {
-  inherit (eval) options;
-}).optionsJSON
+writeShellScriptBin "nixos-docs"
+  # ''
+  #   echo \\\(
+  # ''
+  ''
+    jq -r 'keys - ["_module.args"] | .[]' ${optionsJSON} | fzf --preview='jq -r \'def bold(s): "\u001b[1m\\(s)\u001b[0m"; .["{}"] | "\\(bold("Name")): {}\n\n\\(bold("Description")): \\(.description)\n\n\\(bold("Type")): \\(.type)\n\n\\(bold("Default")): \\(.default.text)\n\n\\(bold("Example")): \\(.example.text)"\' ${optionsJSON}'
+  ''
