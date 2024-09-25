d01788f92c5a970ef42c3009d2f68c56031da91e
Chatnoir Miki <cmiki@amono.me>
2024-09-25 14:47:02 +0800

    fix(caddy): add admin email to generate SSL cert

--- nixosModules/payloads/stellarbase-caddy.nix
+++ nixosModules/payloads/stellarbase-caddy.nix
@@ -2,6 +2,9 @@ _:
 {
   services.caddy = {
     enable = true;
+    globalConfig = ''
+      email cmiki@amono.me
+    '';
     virtualHosts = {
       "photos.amono.me".extraConfig = ''
         reverse_proxy 127.0.0.1:2283
