823809b4af8f65c11923eb4d4d35eeb990aa0c17
Chatnoir Miki <cmiki@amono.me>
2024-09-25 13:28:21 +0800

    feat(nixos): payload system

---
+++ nixosModules/payloads/docker.nix
@@ -0,0 +1,4 @@
+_:
+{
+  virtualisation.docker.enable = true;
+}
--- utils/collect-payloads.nix
+++ utils/collect-payloads.nix
@@ -2,7 +2,7 @@ lib: path:
 let
   nixFilesList = lib.mapAttrsToList
     (fileName: type: {
-      name = lib.removeSuffix ".nix" fileName;
+      name = "payload-" + (lib.removeSuffix ".nix" fileName);
       value = if type == "regular" then path + "/${fileName}" else path + "/${fileName}/default.nix";
     })
     (builtins.readDir path);
