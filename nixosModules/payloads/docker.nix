6e602bb108ac21a0a450322de7d82e86ef29410f
Chatnoir Miki <cmiki@amono.me>
2024-09-25 14:14:06 +0800

    feat(nixos): add newrelic docker agent

--- nixosModules/payloads/docker.nix
+++ nixosModules/payloads/docker.nix
@@ -1,4 +1,20 @@
-_:
+{ lib, config, secrets, ... }:
 {
   virtualisation.docker.enable = true;
+  virtualisation.oci-containers.containers.new-relic-docker-agent = lib.mkIf config.amono.server.newRelicAgent.enable {
+    image = "newrelic/infrastructure:latest";
+    volumes = [
+      "/:/host:ro"
+      "/var/run/docker.sock:/var/run/docker.sock"
+      "newrelic-infra:/etc/newrelic-infra"
+    ];
+    extraOptions = [
+      "--cap-add=SYS_PTRACE"
+      "--privileged"
+      "--pid=host"
+    ];
+    environmentFiles = [
+      "${secrets}/newrelic.env"
+    ];
+  };
 }
