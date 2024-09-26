235b3a1e4d8af44046c8e0803c28aa8ff8b68374
Chatnoir Miki <cmiki@amono.me>
2024-09-26 10:51:03 +0800

    feat(nixos): add docker container collection

--- nixosModules/server/datadog-agent.nix
+++ nixosModules/server/datadog-agent.nix
@@ -6,6 +6,12 @@
     site = "datadoghq.eu";
     apiKeyFile = "${secrets}/dd-api-key.txt";
     enableTraceAgent = true;
-    enableLiveProcessCollection = true;
+    extraConfig = {
+      process_config = {
+        enabled = true;
+        container_collection.enabled = true;
+        process_collection.enabled = true;
+      };
+    };
   };
 }
