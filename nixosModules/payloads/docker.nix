446b8b810c52ed364a51114269518d5c612184f5
Chatnoir Miki <cmiki@amono.me>
2024-09-29 10:52:48 +0800

    feat(nixos): use watchtower to auto-update images

--- nixosModules/payloads/docker.nix
+++ nixosModules/payloads/docker.nix
@@ -1,5 +1,23 @@
-{ lib, config, secrets, ... }:
+{ options, lib, config, secrets, ... }:
+let
+  watchTowerEnable = config.amono.payload.docker.enableWatchTower;
+in
 {
-  virtualisation.docker.enable = true;
-  virtualisation.oci-containers.backend = "docker";
+  options.amono.payload.docker.enableWatchTower = {
+    type = lib.types.bool;
+    default = true;
+    description = "Enable WatchTower to auto update docker images";
+  };
+
+  config = {
+    virtualisation.docker.enable = true;
+    virtualisation.oci-containers.backend = "docker";
+  } // lib.mkIf watchTowerEnable {
+    virtualisation.oci-containers.containers.watch-tower = {
+      image = "containrrr/watchtower";
+      volumes = [
+        "/var/run/docker.sock:/var/run/docker.sock"
+      ];
+    };
+  };
 }
