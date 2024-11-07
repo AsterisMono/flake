d11c744fd7361f54d455063200d18702aad13b46
Chatnoir Miki <cmiki@amono.me>
2024-11-07 08:42:15 +0800

    feat(payload): edit caddy config

--- nixosModules/payloads/stellarbase-caddy.nix
+++ nixosModules/payloads/stellarbase-caddy.nix
@@ -9,6 +9,9 @@ _:
       "photos.amono.me".extraConfig = ''
         reverse_proxy 127.0.0.1:2283
       '';
+      "neko.amono.me".extraConfig = ''
+        reverse_proxy 127.0.0.1:8340
+      '';
     };
   };
 
