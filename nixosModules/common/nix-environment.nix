78e466c42832261f7e35c48f378d9796a152940d
Chatnoir Miki <cmiki@amono.me>
2025-01-16 10:04:25 +0800

    feat(nixos): remove auto optimize store

--- nixosModules/common/nix-environment.nix
+++ nixosModules/common/nix-environment.nix
@@ -35,11 +35,5 @@
     options = "--delete-older-than 1w";
   };
 
-  # Optimise storage
-  # you can also optimise the store manually via:
-  #    nix-store --optimise
-  # https://nixos.org/manual/nix/stable/command-ref/conf-file.html#conf-auto-optimise-store
-  nix.settings.auto-optimise-store = true;
-
   system.stateVersion = "24.05";
 }
