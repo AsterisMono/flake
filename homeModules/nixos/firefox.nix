e28492b0455c0d8fab3d2fa87a351e750a859f52
Chatnoir Miki <cmiki@amono.me>
2025-01-18 10:17:39 +0800

    fix(home): firefox search config being replaced

--- homeModules/nixos/firefox.nix
+++ homeModules/nixos/firefox.nix
@@ -61,18 +61,20 @@
           display: none;
         }
       '';
-      search.default = "Google";
-      search.engines = {
-        "Nix Packages" = {
-          urls = [{
-            template = "https://search.nixos.org/packages";
-            params = [
-              { name = "type"; value = "packages"; }
-              { name = "query"; value = "{searchTerms}"; }
-            ];
-          }];
-          icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-          definedAliases = [ "@np" ];
+      search.force = {
+        default = "Google";
+        engines = {
+          "Nix Packages" = {
+            urls = [{
+              template = "https://search.nixos.org/packages";
+              params = [
+                { name = "type"; value = "packages"; }
+                { name = "query"; value = "{searchTerms}"; }
+              ];
+            }];
+            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = [ "@np" ];
+          };
         };
       };
     };
