61d939a7edaaf6bdc1ef5aed3567c63603541dd1
Chatnoir Miki <cmiki@amono.me>
2025-03-13 13:25:28 +0800

    fix: formatting tool

--- flake.nix
+++ flake.nix
@@ -46,9 +46,15 @@
     stylix.url = "github:danth/stylix/release-24.11";
   };
 
-  outputs = inputs@{ self, ... }:
+  outputs =
+    inputs@{ self, ... }:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
 
       imports = [
         inputs.ilgaak.flakeModule
@@ -56,70 +62,86 @@
 
       ilgaak.enable = false;
 
-      flake =
-        {
-          lib = {
-            bundleModules = import ./utils/bundle-modules.nix inputs.nixpkgs.lib;
-            collectPayloads = import ./utils/collect-payloads.nix inputs.nixpkgs.lib;
-          };
-          nixosConfigurations = import ./configurations/nixos.nix self;
-          darwinConfigurations = import ./configurations/darwin.nix self;
-          nixosModules = {
-            common = self.lib.bundleModules ./nixosModules/common;
-            desktop = self.lib.bundleModules ./nixosModules/desktop;
-            server = self.lib.bundleModules ./nixosModules/server;
-          } // self.lib.collectPayloads ./nixosModules/payloads;
-          darwinModules.darwin = self.lib.bundleModules ./darwinModules;
-          homeModules = {
-            common = self.lib.bundleModules ./homeModules/common;
-            darwin = self.lib.bundleModules ./homeModules/darwin;
-            nixos = self.lib.bundleModules ./homeModules/nixos;
-          };
-          overlays = {
-            flake-packages = import ./overlays/flake-packages.nix self;
-          };
-          deploy = {
-            interactiveSudo = true;
-            nodes = {
-              celestia = {
-                hostname = "celestia";
-                profiles.system = {
-                  user = "root";
-                  path = inputs.deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.celestia;
-                };
+      flake = {
+        lib = {
+          bundleModules = import ./utils/bundle-modules.nix inputs.nixpkgs.lib;
+          collectPayloads = import ./utils/collect-payloads.nix inputs.nixpkgs.lib;
+        };
+        nixosConfigurations = import ./configurations/nixos.nix self;
+        darwinConfigurations = import ./configurations/darwin.nix self;
+        nixosModules = {
+          common = self.lib.bundleModules ./nixosModules/common;
+          desktop = self.lib.bundleModules ./nixosModules/desktop;
+          server = self.lib.bundleModules ./nixosModules/server;
+        } // self.lib.collectPayloads ./nixosModules/payloads;
+        darwinModules.darwin = self.lib.bundleModules ./darwinModules;
+        homeModules = {
+          common = self.lib.bundleModules ./homeModules/common;
+          darwin = self.lib.bundleModules ./homeModules/darwin;
+          nixos = self.lib.bundleModules ./homeModules/nixos;
+        };
+        overlays = {
+          flake-packages = import ./overlays/flake-packages.nix self;
+        };
+        deploy = {
+          interactiveSudo = true;
+          nodes = {
+            celestia = {
+              hostname = "celestia";
+              profiles.system = {
+                user = "root";
+                path = inputs.deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.celestia;
               };
-              stellarbase = {
-                hostname = "stellarbase";
-                profiles.system = {
-                  user = "root";
-                  path = inputs.deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.stellarbase;
-                };
+            };
+            stellarbase = {
+              hostname = "stellarbase";
+              profiles.system = {
+                user = "root";
+                path = inputs.deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.stellarbase;
               };
             };
           };
-
-          # This is highly advised, and will prevent many possible mistakes
-          checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) inputs.deploy-rs.lib;
         };
 
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
-        checks = {
-          pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
-            src = ./.;
-            hooks = {
-              nixpkgs-fmt.enable = true;
-              statix.enable = true;
-            };
-          };
-        };
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [ nixd just deploy-rs nh ];
-          inherit (self'.checks.pre-commit-check) shellHook;
-          buildInputs = self'.checks.pre-commit-check.enabledPackages;
-        };
-        packages = {
-          torus-font = pkgs.callPackage ./packages/torus { };
-        };
+        # This is highly advised, and will prevent many possible mistakes
+        checks = builtins.mapAttrs (
+          system: deployLib: deployLib.deployChecks self.deploy
+        ) inputs.deploy-rs.lib;
       };
+
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          checks = {
+            pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+              src = ./.;
+              hooks = {
+                nixfmt-rfc-style.enable = true;
+                statix.enable = true;
+              };
+            };
+          };
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              nixd
+              just
+              deploy-rs
+              nh
+              nixfmt-rfc-style
+            ];
+            inherit (self'.checks.pre-commit-check) shellHook;
+            buildInputs = self'.checks.pre-commit-check.enabledPackages;
+          };
+          packages = {
+            torus-font = pkgs.callPackage ./packages/torus { };
+          };
+        };
     };
 }
