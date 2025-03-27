9dddfc23a949283b6601e9978cfb9bd8f42ccfe5
Chatnoir Miki <cmiki@amono.me>
2025-03-27 21:36:29 +0800

    feat: set noProxy for minikube

--- nixosModules/common/proxy.nix
+++ nixosModules/common/proxy.nix
@@ -30,6 +30,7 @@ in
       inherit httpProxy;
       httpsProxy = httpProxy;
       allProxy = socksProxy;
+      noProxy = "localhost,127.0.0.1,10.96.0.0/12,192.168.59.0/24,192.168.49.0/24,192.168.39.0/24"; # minikube
     };
     # iptables based firewall needs to be disabled
     # NixOS employs firewall by default
