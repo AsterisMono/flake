f424141c973ab7fae7c65856cb04c141e0adc066
Chatnoir Miki <cmiki@amono.me>
2025-04-15 10:03:55 +0800

    feat: update ivy static ip

--- configurations/hardwares/ivy.nix
+++ configurations/hardwares/ivy.nix
@@ -25,7 +25,22 @@
 
   swapDevices = [ ];
 
-  networking.useDHCP = lib.mkDefault true;
+  networking = {
+    useDHCP = false;
+    interfaces.enp6s18.ipv4 = {
+      addresses = [
+        {
+          address = "192.168.31.100";
+          prefixLength = 24;
+        }
+      ];
+    };
+    defaultGateway = {
+      address = "192.168.31.1";
+      interface = "enp6s18";
+    };
+    nameservers = [ "114.114.114.114" ];
+  };
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
