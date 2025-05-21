6dc9b5124fe706920384244a900b30620bcc3c31
Noa Virellia <cmiki@amono.me>
2025-05-21 14:38:47 +0800

    feat: add template support

---
+++ templates/devShell/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Basic flake for devShell";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            # devShell packages
+          ];
+        };
+      }
+    );
+}
