8a40dfdec2b0df2e243edb7ba5dd44d49549a573
Noa Virellia <cmiki@amono.me>
2025-06-13 16:22:21 +0800

    feat: add mdns service

---
+++ nixosModules/services/mdns.nix
@@ -0,0 +1,12 @@
+_: {
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      userServices = true;
+      workstation = true;
+    };
+  };
+}
