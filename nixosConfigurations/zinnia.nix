7110ac8cd09b81bcf16f2f2d00f867b2f1e7356f
Noa Virellia <cmiki@amono.me>
2025-06-16 17:27:44 +0800

    feat: add ci github runner

--- nixosConfigurations/zinnia.nix
+++ nixosConfigurations/zinnia.nix
@@ -1,6 +1,8 @@
 {
+  config,
   nixosModules,
   homeModules,
+  secrets,
   ...
 }:
 {
@@ -14,6 +16,23 @@
   ];
 
   disko.devices.disk.main.device = "/dev/sda";
+
+  sops.secrets.ci_runner_token = {
+    format = "yaml";
+    sopsFile = "${secrets}/github.yaml";
+    restartUnits = [
+      "github-runner-irrigation.service"
+    ];
+  };
+
+  services.github-runners = {
+    irrigation = {
+      enable = true;
+      name = "irrigation";
+      tokenFile = config.sops.secrets.ci_runner_token.path;
+      url = "https://github.com/AsterisMono/flake";
+    };
+  };
 
   noa = {
     nix.enableUSTCSubstituter = true;
