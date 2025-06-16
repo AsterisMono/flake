53b5507e11ede3fe7711dffc611ba032035f6b37
Noa Virellia <cmiki@amono.me>
2025-06-16 12:23:05 +0800

    chore: update ssh key

--- lib/withOfflineInstaller.nix
+++ lib/withOfflineInstaller.nix
@@ -62,7 +62,7 @@ nixosConfig
               shell = pkgs.bashInteractive;
               initialHashedPassword = lib.mkForce "$y$j9T$Or7mqutFE5iEFtJb4QmdR1$N0yuyRzIOavwnsnrkK4yR5Msg1oQ0RAXpKVN/LpV3p.";
               openssh.authorizedKeys.keys = lib.mkForce [
-                "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMGPXhPWInP+CEc8wd+BWUiAqIAAIbF6rYuoZkt0QNiH"
+                "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOOz0CMmkGSXv4H77rmrmvadltAlwAZeVimxGoUAfArs"
               ];
             };
           }
