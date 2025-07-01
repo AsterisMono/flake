26090ae903203512a2c5d832fac9c0de182ea025
Noa Virellia <cmiki@amono.me>
2025-07-01 16:45:45 +0800

    feat: enable pocket-id on seralia

--- nixosConfigurations/seralia.nix
+++ nixosConfigurations/seralia.nix
@@ -1,5 +1,7 @@
 {
+  config,
   nixosModules,
+  secretsPath,
   ...
 }:
 {
@@ -8,14 +10,40 @@
     roles.server
     services.tailscale
     services.ssh
+    services.docker
   ];
 
   disko.devices.disk.main.device = "/dev/vda";
 
-  networking.firewall.allowedTCPPorts = [
-    6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
-  ];
+  noa.docker.enableWatchTower = true;
 
-  services.k3s.enable = true;
-  services.k3s.role = "server";
+  sops.secrets.pocket-id = {
+    sopsFile = "${secretsPath}/pocketId.env";
+    format = "dotenv";
+    key = "";
+  };
+
+  virtualisation.oci-containers.containers.pocket-id = {
+    image = "ghcr.io/pocket-id/pocket-id:v1";
+    volumes = [ "/var/lib/pocketid:/app/data" ];
+    ports = [ "127.0.0.1:1411:1411" ];
+    environmentFiles = [ config.sops.secrets.pocket-id.path ];
+  };
+
+  services.caddy = {
+    enable = true;
+    globalConfig = ''
+      email cmiki@amono.me
+    '';
+    virtualHosts = {
+      "citadel.requiem.garden".extraConfig = ''
+        reverse_proxy 127.0.0.1:1411
+      '';
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [
+    80
+    443
+  ];
 }
