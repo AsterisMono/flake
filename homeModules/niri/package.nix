a6a9e493d7ea0b0c1285ab8152d99a7962696257
Noa Virellia <cmiki@amono.me>
2025-08-27 10:26:13 +0800

    feat(niri): startup / workspace

--- homeModules/niri/package.nix
+++ homeModules/niri/package.nix
@@ -3,6 +3,7 @@
   lib,
   pkgs,
   assetsPath,
+  hostname,
   ...
 }:
 # https://github.com/jetjinser/flake/blob/master/hosts/dorothy/desktop/niri.nix
@@ -11,233 +12,329 @@
 {
   imports = [ ../utils/wayland.nix ];
 
-  programs.niri = {
-    package = pkgs.niri;
-    settings = {
-      environment = {
-        LANG = "zh_CN.UTF-8";
-        LANGUAGE = "zh_CN:en_US";
+  programs.niri =
+    let
+      monitors = {
+        gpdBuiltin = "Japan Display Inc. GPD1001H 0x00000001";
+        aocLeft = "PNP(AOC) U27V4 VDOP5HA000781";
+        aocRight = "PNP(AOC) U27V4 HDKM3HA008442";
+        xiaomi = "Xiaomi Corporation Mi Monitor 5877500021251";
       };
-
-      hotkey-overlay.skip-at-startup = true;
-      prefer-no-csd = true;
-
-      outputs = {
-        "Xiaomi Corporation Mi Monitor 5877500021251".scale = 1.75;
-        "PNP(AOC) U27V4 VDOP5HA000781" = {
-          scale = 1.5;
-          position.x = 0;
-          position.y = 0;
-        };
-        "PNP(AOC) U27V4 HDKM3HA008442" = {
-          scale = 1.5;
-          position.x = 2560;
-          position.y = 0;
-        };
-        "Japan Display Inc. GPD1001H 0x00000001" = {
-          scale = 2;
-          position.x = 3840;
-          position.y = 1440;
-        };
-      };
-
-      layout = {
-        default-column-width.proportion = 0.5;
-        # https://github.com/YaLTeR/niri/wiki/Overview#backdrop-customization
-        background-color = "transparent";
-        always-center-single-column = true;
-        focus-ring = {
-          width = 2;
-          active.color = "#9ccfd8";
-          urgent.color = "#eb6f92";
-        };
-      };
-
-      cursor = {
-        hide-when-typing = true;
-      };
-
-      input = {
-        workspace-auto-back-and-forth = true;
-      };
-
-      binds =
-        with config.lib.niri.actions;
-        let
-          sh = spawn "sh" "-c";
-          wpctl = lib.getExe' pkgs.wireplumber "wpctl";
-        in
-        {
-          "Mod+D".action = spawn "fuzzel";
-          "Mod+Q".action = spawn "alacritty";
-          "Mod+C" = {
-            action = close-window;
-            repeat = false;
-          };
-          "Mod+O" = {
-            action = toggle-overview;
-            repeat = false;
-          };
-
-          "Mod+H".action = focus-column-left;
-          "Mod+J".action = focus-window-or-workspace-down;
-          "Mod+K".action = focus-window-or-workspace-up;
-          "Mod+L".action = focus-column-right;
-
-          "Mod+Shift+H".action = move-column-left;
-          "Mod+Shift+J".action = move-window-down;
-          "Mod+Shift+K".action = move-window-up;
-          "Mod+Shift+L".action = move-column-right;
-
-          "Mod+Ctrl+H".action = focus-monitor-left;
-          "Mod+Ctrl+J".action = focus-monitor-down;
-          "Mod+Ctrl+K".action = focus-monitor-up;
-          "Mod+Ctrl+L".action = focus-monitor-right;
-
-          "Mod+Shift+Ctrl+H".action = move-column-to-monitor-left;
-          "Mod+Shift+Ctrl+J".action = move-column-to-monitor-down;
-          "Mod+Shift+Ctrl+K".action = move-column-to-monitor-up;
-          "Mod+Shift+Ctrl+L".action = move-column-to-monitor-right;
-
-          "Mod+U".action = focus-workspace-down;
-          "Mod+I".action = focus-workspace-up;
-          "Mod+Shift+U".action = move-column-to-workspace-down;
-          "Mod+Shift+I".action = move-column-to-workspace-up;
-          "Mod+WheelScrollDown" = {
-            action = focus-workspace-down;
-            cooldown-ms = 150;
-          };
-          "Mod+WheelScrollUp" = {
-            action = focus-workspace-up;
-            cooldown-ms = 150;
-          };
-          "Mod+Ctrl+WheelScrollDown" = {
-            action = move-column-to-workspace-down;
-            cooldown-ms = 150;
-          };
-          "Mod+Ctrl+WheelScrollUp" = {
-            action = move-column-to-workspace-up;
-            cooldown-ms = 150;
-          };
-          "Mod+Shift+WheelScrollDown".action = focus-column-right;
-          "Mod+Shift+WheelScrollUp".action = focus-column-left;
-          "Mod+Ctrl+Shift+WheelScrollDown".action = move-column-right;
-          "Mod+Ctrl+Shift+WheelScrollUp".action = move-column-left;
-
-          "Mod+BracketLeft".action = consume-or-expel-window-left;
-          "Mod+BracketRight".action = consume-or-expel-window-right;
-          "Mod+Comma".action = consume-window-into-column;
-          "Mod+Period".action = expel-window-from-column;
-
-          "Mod+R".action = switch-preset-column-width;
-          "Mod+Shift+R".action = switch-preset-window-height;
-          "Mod+Ctrl+R".action = reset-window-height;
-          "Mod+F".action = maximize-column;
-          "Mod+Shift+F".action = fullscreen-window;
-          "Mod+Ctrl+F".action = expand-column-to-available-width;
-          "Mod+Shift+C".action = center-column;
-          "Mod+Ctrl+C".action = center-visible-columns;
-
-          "Mod+Minus".action = set-column-width "-10%";
-          "Mod+Equal".action = set-column-width "+10%";
-          "Mod+Shift+Minus".action = set-window-height "-10%";
-          "Mod+Shift+Equal".action = set-window-height "+10%";
-
-          "Mod+V".action = toggle-window-floating;
-          "Mod+Shift+V".action = switch-focus-between-floating-and-tiling;
-
-          "Mod+Shift+S".action = screenshot;
-          "Mod+Shift+P".action = screenshot-window;
-          # "Mod+Shift+Ctrl+P".action = screenshot-screen;
-
-          "Alt+V".action =
-            let
-              cliphist-fuzzel = pkgs.writeShellApplication {
-                name = "cliphist-fuzzel";
-                runtimeInputs = with pkgs; [
-                  cliphist
-                  imagemagick
-                  fuzzel
-                  gawk
-                ];
-                bashOptions = [
-                  "nounset"
-                  "pipefail"
-                ];
-                text = builtins.readFile ./cliphist-fuzzel.sh;
-              };
-            in
-            sh (lib.getExe cliphist-fuzzel);
-
-          "Mod+Shift+E".action = quit;
-
-          "XF86AudioRaiseVolume" = {
-            action = sh "${wpctl} set-volume @DEFAULT_AUDIO_SINK@ 10%+";
-            allow-when-locked = true;
-          };
-          "XF86AudioLowerVolume" = {
-            action = sh "${wpctl} set-volume @DEFAULT_AUDIO_SINK@ 10%-";
-            allow-when-locked = true;
-          };
-          "XF86AudioMute" = {
-            action = sh "${wpctl} set-mute @DEFAULT_AUDIO_SINK@ toggle";
-            allow-when-locked = true;
-          };
-
-          "Ctrl+Mod+Delete".action = sh (lib.getExe pkgs.swaylock-effects);
+      isAeris = hostname == "aeris";
+    in
+    {
+      package = pkgs.niri;
+      settings = {
+        environment = {
+          LANG = "zh_CN.UTF-8";
+          LANGUAGE = "zh_CN:en_US";
         };
 
-      layer-rules = [
-        {
-          matches = [ { namespace = "^notifications$"; } ];
-          block-out-from = "screencast";
-        }
-        # https://github.com/sodiboo/niri-flake/pull/1063
-        {
-          matches = [ { namespace = "^wallpaper$"; } ];
-          place-within-backdrop = true;
-        }
-      ];
+        hotkey-overlay.skip-at-startup = true;
+        prefer-no-csd = true;
 
-      window-rules = [
-        {
-          geometry-corner-radius = {
-            bottom-left = 8.0;
-            bottom-right = 8.0;
-            top-left = 8.0;
-            top-right = 8.0;
+        outputs = {
+          "${monitors.xiaomi}" = {
+            scale = 1.75;
+            focus-at-startup = true;
           };
-          clip-to-geometry = true;
-        }
-        {
-          matches = [
-            { app-id = "^org\.telegram\.desktop$"; }
-            { app-id = "^QQ$"; }
-          ];
-          block-out-from = "screencast";
-        }
-        {
-          matches = [
-            {
-              app-id = "^QQ$";
-              title = "^(图片查看)|(视频播放)器$";
-            }
-            {
-              app-id = "^org\.telegram\.desktop$";
-              title = "^Media viewer$";
-            }
-            {
-              app-id = "^firefox$";
-              title = "^Picture-in-Picture$";
-            }
-          ];
-          open-floating = true;
-          default-window-height.proportion = 0.65;
-          default-column-width.proportion = 0.65;
-        }
-      ];
+          "${monitors.aocLeft}" = {
+            scale = 1.5;
+            position.x = 0;
+            position.y = 0;
+            focus-at-startup = true;
+          };
+          "${monitors.aocRight}" = {
+            scale = 1.5;
+            position.x = 2560;
+            position.y = 0;
+          };
+          "${monitors.gpdBuiltin}" = {
+            scale = 2;
+            position.x = 3840;
+            position.y = 1440;
+          };
+        };
+
+        layout = {
+          default-column-width.proportion = 0.5;
+          # https://github.com/YaLTeR/niri/wiki/Overview#backdrop-customization
+          background-color = "transparent";
+          always-center-single-column = true;
+          focus-ring = {
+            width = 2;
+            active.color = "#9ccfd8";
+            urgent.color = "#eb6f92";
+            inactive.color = "#e0def4";
+          };
+        };
+
+        cursor = {
+          hide-when-typing = true;
+        };
+
+        input = {
+          workspace-auto-back-and-forth = true;
+        };
+
+        binds =
+          with config.lib.niri.actions;
+          let
+            sh = spawn "sh" "-c";
+            wpctl = lib.getExe' pkgs.wireplumber "wpctl";
+          in
+          {
+            "Mod+D".action = spawn "fuzzel";
+            "Mod+Q".action = spawn "alacritty";
+            "Mod+C" = {
+              action = close-window;
+              repeat = false;
+            };
+            "Mod+O" = {
+              action = toggle-overview;
+              repeat = false;
+            };
+
+            "Mod+H".action = focus-column-left;
+            "Mod+J".action = focus-window-or-workspace-down;
+            "Mod+K".action = focus-window-or-workspace-up;
+            "Mod+L".action = focus-column-right;
+
+            "Mod+Shift+H".action = move-column-left;
+            "Mod+Shift+J".action = move-window-down;
+            "Mod+Shift+K".action = move-window-up;
+            "Mod+Shift+L".action = move-column-right;
+
+            "Mod+Ctrl+H".action = focus-monitor-left;
+            "Mod+Ctrl+J".action = focus-monitor-down;
+            "Mod+Ctrl+K".action = focus-monitor-up;
+            "Mod+Ctrl+L".action = focus-monitor-right;
+
+            "Mod+Shift+Ctrl+H".action = move-column-to-monitor-left;
+            "Mod+Shift+Ctrl+J".action = move-column-to-monitor-down;
+            "Mod+Shift+Ctrl+K".action = move-column-to-monitor-up;
+            "Mod+Shift+Ctrl+L".action = move-column-to-monitor-right;
+
+            "Mod+U".action = focus-workspace-down;
+            "Mod+I".action = focus-workspace-up;
+            "Mod+Shift+U".action = move-column-to-workspace-down;
+            "Mod+Shift+I".action = move-column-to-workspace-up;
+            "Mod+WheelScrollDown" = {
+              action = focus-workspace-down;
+              cooldown-ms = 150;
+            };
+            "Mod+WheelScrollUp" = {
+              action = focus-workspace-up;
+              cooldown-ms = 150;
+            };
+            "Mod+Ctrl+WheelScrollDown" = {
+              action = move-column-to-workspace-down;
+              cooldown-ms = 150;
+            };
+            "Mod+Ctrl+WheelScrollUp" = {
+              action = move-column-to-workspace-up;
+              cooldown-ms = 150;
+            };
+            "Mod+Shift+WheelScrollDown".action = focus-column-right;
+            "Mod+Shift+WheelScrollUp".action = focus-column-left;
+            "Mod+Ctrl+Shift+WheelScrollDown".action = move-column-right;
+            "Mod+Ctrl+Shift+WheelScrollUp".action = move-column-left;
+
+            "Mod+BracketLeft".action = consume-or-expel-window-left;
+            "Mod+BracketRight".action = consume-or-expel-window-right;
+            "Mod+Comma".action = consume-window-into-column;
+            "Mod+Period".action = expel-window-from-column;
+
+            "Mod+R".action = switch-preset-column-width;
+            "Mod+Shift+R".action = switch-preset-window-height;
+            "Mod+Ctrl+R".action = reset-window-height;
+            "Mod+F".action = maximize-column;
+            "Mod+Shift+F".action = fullscreen-window;
+            "Mod+Ctrl+F".action = expand-column-to-available-width;
+            "Mod+Shift+C".action = center-column;
+            "Mod+Ctrl+C".action = center-visible-columns;
+
+            "Mod+Minus".action = set-column-width "-10%";
+            "Mod+Equal".action = set-column-width "+10%";
+            "Mod+Shift+Minus".action = set-window-height "-10%";
+            "Mod+Shift+Equal".action = set-window-height "+10%";
+
+            "Mod+V".action = toggle-window-floating;
+            "Mod+Shift+V".action = switch-focus-between-floating-and-tiling;
+
+            "Mod+Shift+S".action = screenshot;
+            "Mod+Shift+P".action = screenshot-window;
+            # "Mod+Shift+Ctrl+P".action = screenshot-screen;
+
+            "Alt+V".action =
+              let
+                cliphist-fuzzel = pkgs.writeShellApplication {
+                  name = "cliphist-fuzzel";
+                  runtimeInputs = with pkgs; [
+                    cliphist
+                    imagemagick
+                    fuzzel
+                    gawk
+                  ];
+                  bashOptions = [
+                    "nounset"
+                    "pipefail"
+                  ];
+                  text = builtins.readFile ./cliphist-fuzzel.sh;
+                };
+              in
+              sh (lib.getExe cliphist-fuzzel);
+
+            "Mod+Shift+E".action = quit;
+
+            "XF86AudioRaiseVolume" = {
+              action = sh "${wpctl} set-volume @DEFAULT_AUDIO_SINK@ 10%+";
+              allow-when-locked = true;
+            };
+            "XF86AudioLowerVolume" = {
+              action = sh "${wpctl} set-volume @DEFAULT_AUDIO_SINK@ 10%-";
+              allow-when-locked = true;
+            };
+            "XF86AudioMute" = {
+              action = sh "${wpctl} set-mute @DEFAULT_AUDIO_SINK@ toggle";
+              allow-when-locked = true;
+            };
+
+            "Ctrl+Mod+Delete".action = sh (lib.getExe pkgs.swaylock-effects);
+          };
+
+        layer-rules = [
+          {
+            matches = [ { namespace = "^notifications$"; } ];
+            block-out-from = "screencast";
+          }
+          # https://github.com/sodiboo/niri-flake/pull/1063
+          {
+            matches = [ { namespace = "^wallpaper$"; } ];
+            place-within-backdrop = true;
+          }
+        ];
+
+        window-rules = [
+          {
+            geometry-corner-radius = {
+              bottom-left = 8.0;
+              bottom-right = 8.0;
+              top-left = 8.0;
+              top-right = 8.0;
+            };
+            clip-to-geometry = true;
+          }
+          {
+            matches = [
+              { app-id = "^org\.telegram\.desktop$"; }
+              { app-id = "^QQ$"; }
+            ];
+            block-out-from = "screencast";
+          }
+          {
+            matches = [
+              {
+                app-id = "^QQ$";
+                title = "^(图片查看)|(视频播放)器$";
+              }
+              {
+                app-id = "^org\.telegram\.desktop$";
+                title = "^Media viewer$";
+              }
+              {
+                app-id = "^firefox$";
+                title = "^Picture-in-Picture$";
+              }
+            ];
+            open-floating = true;
+            default-window-height.proportion = 0.65;
+            default-column-width.proportion = 0.65;
+          }
+
+        ]
+        ++ lib.optionals isAeris [
+          {
+            matches = [
+              { app-id = "^firefox$"; }
+            ];
+            open-on-workspace = "01-browser";
+            default-column-width.proportion = 1.0;
+          }
+          {
+            matches = [
+              { app-id = "^Slack$"; }
+              { app-id = "^org\.telegram\.desktop$"; }
+            ];
+            open-on-workspace = "02-chat";
+            default-column-width.proportion = 0.5;
+          }
+          {
+            matches = [
+              { app-id = "^Bitwarden$"; }
+            ];
+            open-on-workspace = "03-misc";
+            default-column-width.proportion = 0.5;
+          }
+          {
+            matches = [
+              {
+                app-id = "^Alacritty$";
+                title = "^btop$";
+              }
+            ];
+            open-on-workspace = "01-sysmon";
+            default-column-width.proportion = 1.0;
+          }
+          {
+            matches = [
+              { app-id = "^code$"; }
+            ];
+            open-on-workspace = "01-code";
+            default-column-width.proportion = 0.5;
+          }
+          {
+            matches = builtins.map (l: l // { at-startup = true; }) [
+              { app-id = "^firefox$"; }
+              { app-id = "^Slack$"; }
+              { app-id = "^org\.telegram\.desktop$"; }
+              { app-id = "^code$"; }
+              {
+                app-id = "^Alacritty$";
+                title = "^btop$";
+              }
+              { app-id = "^Bitwarden$"; }
+            ];
+            open-focused = false;
+          }
+        ];
+
+        workspaces = lib.optionalAttrs isAeris {
+          "01-browser".open-on-output = monitors.aocLeft;
+          "02-chat".open-on-output = monitors.aocLeft;
+          "03-misc".open-on-output = monitors.aocLeft;
+          "01-code".open-on-output = monitors.aocRight;
+          "01-sysmon".open-on-output = monitors.gpdBuiltin;
+        };
+
+        spawn-at-startup = lib.optionals isAeris [
+          { command = [ "firefox" ]; }
+          { command = [ "slack" ]; }
+          { command = [ "Telegram" ]; }
+          {
+            command = [
+              "alacritty"
+              "-T"
+              "btop"
+              "-e"
+              "btop"
+            ];
+          }
+          { command = [ "code" ]; }
+          { command = [ "bitwarden" ]; }
+        ];
+      };
     };
-  };
 
   programs.fuzzel = {
     enable = true;
