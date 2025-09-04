a52eed2a2f5fdeff20fd3300a09f807b0af4e98f
Noa Virellia <cmiki@amono.me>
2025-09-04 16:55:11 +0800

    feat(home/kitty): override font size

--- homeModules/niri/package.nix
+++ homeModules/niri/package.nix
@@ -317,6 +317,7 @@
   programs.kitty = {
     enable = true;
     settings = {
+      font_size = lib.mkForce 11.0;
       cursor_shape = "beam";
       cursor_blink_interval = 0;
       cursor_beam_thickness = 1.2;
