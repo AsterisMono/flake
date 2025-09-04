38c370aa480ac673b0c5555f28083aae44a30fec
Noa Virellia <cmiki@amono.me>
2025-09-04 08:54:54 +0800

    feat(config/ivy): add peer cryolitia

--- nixosConfigurations/ivy.nix
+++ nixosConfigurations/ivy.nix
@@ -60,6 +60,12 @@
         tunnelLocalAddr = "ee80:759::0833/64";
         tunnelPeerAddr = "ee80:759::2323/64";
       };
+      "AS4242420994" = {
+        endpoint = "***REMOVED***";
+        pubkey = "***REMOVED***";
+        tunnelLocalAddr = "ee80:5::0833/64";
+        tunnelPeerAddr = "ee80:5::0994/64";
+      };
     };
   };
 }
