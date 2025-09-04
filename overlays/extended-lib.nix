21af9fb5540f675b810d3e4a1a8101a51bb60187
Noa Virellia <cmiki@amono.me>
2025-09-04 13:35:21 +0800

    feat(flake): introduce nixpak input and lib overlay

--- overlays/extended-lib.nix
+++ overlays/extended-lib.nix
@@ -1,5 +1,9 @@
 flake: final: prev: {
   lib = prev.lib // {
     wrapped = flake.inputs.wrapper-manager.lib.wrapWith final;
+    mkNixPak = flake.inputs.nixpak.lib.nixpak {
+      inherit (prev) lib;
+      pkgs = prev;
+    };
   };
 }
