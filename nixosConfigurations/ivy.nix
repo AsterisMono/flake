ebe6249a070a5a07f3580021e37edbf5483d392b
Noa Virellia <cmiki@amono.me>
2025-09-04 12:06:34 +0800

    chore(config/ivy): update as4242420994 tunnel addresses

--- nixosConfigurations/ivy.nix
+++ nixosConfigurations/ivy.nix
@@ -63,8 +63,8 @@
       "AS4242420994" = {
         endpoint = "hgt0ae5n23e.sn.mynetname.net:20833";
         pubkey = "oJStkRIo8cO4IGj/+Tdi2FcK/eDg1NR4dF9ZNIqPyVg=";
-        tunnelLocalAddr = "ee80:5::0833/64";
-        tunnelPeerAddr = "ee80:5::0994/64";
+        tunnelLocalAddr = "fdd2:4372:796f:ffff::833:1/127";
+        tunnelPeerAddr = "fdd2:4372:796f:ffff::833:0/127";
       };
     };
   };
