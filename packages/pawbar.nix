80ffeadcda5d3f8f2dee764a91a2d668f2502ea8
Noa Virellia <cmiki@amono.me>
2025-09-05 16:38:38 +0800

    fix(packages/vaxis): manually download vaxis from sourcehut

--- packages/pawbar.nix
+++ packages/pawbar.nix
@@ -1,5 +1,6 @@
 {
   fetchFromGitHub,
+  fetchgit,
   buildGoModule,
 
   udev,
@@ -7,7 +8,12 @@
   cairo,
   pkg-config,
 }:
-
+let
+  vaxis = fetchgit {
+    url = "https://git.sr.ht/~codelif/vaxis";
+    hash = "sha256-7j9npVYu7U8QyXrFYbeyda64l0S0VYdQNE9S6wc+lxM=";
+  };
+in
 buildGoModule {
   pname = "pawbar";
   version = "0-unstable-2025-08-31";
@@ -17,6 +23,10 @@ buildGoModule {
     rev = "0963638b92a5206904ad85f95760177683cea383";
     hash = "sha256-dfQwCLXVGMkMff/uiDKQptDIGZbilob68X3lI57Tt48=";
   };
+
+  postUnpack = ''
+    cp -r ${vaxis} vaxis
+  '';
 
   subPackages = [ "cmd/pawbar" ];
   vendorHash = "sha256-5ysy7DGLE99svDPUw1vS05CT7HRcSP1ov27rTqm6a8Y=";
