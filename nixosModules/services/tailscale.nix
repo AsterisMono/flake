f5cbe0937dd53b903b3365c959b5c4cc6e72d69b
Noa Virellia <cmiki@amono.me>
2025-09-06 18:33:05 +0800

    fix(nixos/tailscale): introduce kernel patch to mitigate tailscale regression

--- nixosModules/services/tailscale.nix
+++ nixosModules/services/tailscale.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  pkgs,
   config,
   secretsPath,
   ...
@@ -71,5 +72,19 @@
       trustedInterfaces = [ "tailscale0" ];
       allowedUDPPorts = [ config.services.tailscale.port ];
     };
+
+    boot.kernelPatches = [
+      # Fix the /proc/net/tcp seek issue
+      # Impacts tailscale: https://github.com/tailscale/tailscale/issues/16966
+      {
+        name = "proc: fix missing pde_set_flags() for net proc files";
+        patch = pkgs.fetchurl {
+          name = "fix-missing-pde_set_flags-for-net-proc-files.patch";
+          url = "https://patchwork.kernel.org/project/linux-fsdevel/patch/20250821105806.1453833-1-wangzijie1@honor.com/raw/";
+          hash = "sha256-DbQ8FiRj65B28zP0xxg6LvW5ocEH8AHOqaRbYZOTDXg=";
+        };
+      }
+    ];
+
   };
 }
