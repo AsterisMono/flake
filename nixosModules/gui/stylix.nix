aed8238784e08cf9ae60dd5e99a48fab0765a720
Noa Virellia <cmiki@amono.me>
2025-09-08 10:25:49 +0800

    feat(nixos/stylix): plymouth animation

--- nixosModules/gui/stylix.nix
+++ nixosModules/gui/stylix.nix
@@ -1,9 +1,28 @@
 { pkgs, assetsPath, ... }:
 {
+  boot = {
+    plymouth.enable = true;
+    # Enable "Silent boot"
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "udev.log_priority=3"
+      "rd.systemd.show_status=auto"
+    ];
+    # Hide the OS choice for bootloaders.
+    # It's still possible to open the bootloader list by pressing any key
+    # It will just not appear on screen unless a key is pressed
+    loader.timeout = 0;
+  };
+
   stylix = {
     enable = true;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
     image = "${assetsPath}/wallpapers/reisa.jpg";
+    targets.plymouth.logo = "${assetsPath}/nix-snowflake-transgender.png";
     fonts = {
       serif = {
         package = pkgs.noto-fonts-cjk-serif;
