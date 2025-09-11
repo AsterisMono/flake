a64307b9d71a1298208b634ea45e0ad34072d4c0
Noa Virellia <cmiki@amono.me>
2025-09-11 10:04:01 +0800

    feat(home/waybar): use a fork to test systemd-failed-unit bug resolution

--- homeModules/niri/waybar/default.nix
+++ homeModules/niri/waybar/default.nix
@@ -1,6 +1,16 @@
-_: {
+{ pkgs, ... }:
+{
   programs.waybar = {
     enable = true;
+    package = pkgs.waybar.overrideAttrs (old: {
+      version = "0.14.0";
+      src = pkgs.fetchFromGitHub {
+        owner = "stkth";
+        repo = "Waybar";
+        rev = "3060141a65fad5a79c7f8375c1e363d1e6e34f58";
+        hash = "sha256-OJ8fK4KzIfh1nQphZOlQj/iBDlh7AK9LDPxuneJ7k6w=";
+      };
+    });
     systemd.enable = true;
     style = ./style.css;
   };
