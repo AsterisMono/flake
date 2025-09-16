d5464fee05fb8a902e090b8e7a98dd6c72c1a80e
Noa Virellia <cmiki@amono.me>
2025-09-16 20:47:15 +0800

    feat(home/kitty): neovim integration

--- homeModules/niri/package.nix
+++ homeModules/niri/package.nix
@@ -364,7 +364,20 @@
       cursor_shape = "beam";
       cursor_blink_interval = 0;
       cursor_beam_thickness = 1.2;
+      allow_remote_control = "socket-only";
+      listen_on = "unix:/tmp/kitty";
+      shell_integration = "enabled";
     };
+    extraConfig = ''
+      # kitty-scrollback.nvim Kitten alias
+      action_alias kitty_scrollback_nvim kitten /home/cmiki/.local/share/nvim/lazy/kitty-scrollback.nvim/python/kitty_scrollback_nvim.py
+      # Browse scrollback buffer in nvim
+      map kitty_mod+h kitty_scrollback_nvim
+      # Browse output of the last shell command in nvim
+      map kitty_mod+g kitty_scrollback_nvim --config ksb_builtin_last_cmd_output
+      # Show clicked command output in nvim
+      mouse_map ctrl+shift+right press ungrabbed combine : mouse_select_command_output : kitty_scrollback_nvim --config ksb_builtin_last_visited_cmd_output
+    '';
   };
 
   services.mako.enable = true;
