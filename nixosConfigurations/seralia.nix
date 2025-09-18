f17cce8cf19a46b4348abb5c59b558af06441f40
Noa Virellia <cmiki@amono.me>
2025-09-18 12:34:29 +0800

    feat(config/seralia): repurpose seralia

--- nixosConfigurations/seralia.nix
+++ nixosConfigurations/seralia.nix
@@ -1,44 +1,74 @@
 {
   config,
   nixosModules,
-  secretsPath,
   ...
 }:
+let
+  grafanaDomain = "observatory.requiem.garden";
+in
 {
   imports = with nixosModules; [
     diskLayouts.btrfs
     roles.server
     services.tailscale
-    services.docker
   ];
 
   disko.devices.disk.main.device = "/dev/vda";
 
-  noa.docker.enableWatchTower = true;
-
-  sops.secrets.pocket-id = {
-    sopsFile = "${secretsPath}/pocketId.env";
-    format = "dotenv";
-    key = "";
-  };
-
-  virtualisation.oci-containers.containers.pocket-id = {
-    image = "ghcr.io/pocket-id/pocket-id:v1";
-    volumes = [ "/var/lib/pocketid:/app/data" ];
-    ports = [ "127.0.0.1:1411:1411" ];
-    environmentFiles = [ config.sops.secrets.pocket-id.path ];
-  };
-
-  services.caddy = {
+  services.prometheus = {
     enable = true;
-    globalConfig = ''
-      email cmiki@amono.me
-    '';
-    virtualHosts = {
-      "citadel.requiem.garden".extraConfig = ''
-        reverse_proxy 127.0.0.1:1411
-      '';
+    globalConfig.scrape_interval = "10s";
+    exporters.node = {
+      enable = true;
+      enabledCollectors = [
+        "systemd"
+      ];
+      disabledCollectors = [ "textfile" ];
     };
+    scrapeConfigs = [
+      {
+        job_name = "node";
+        static_configs = [
+          {
+            targets = [
+              "localhost:${toString config.services.prometheus.exporters.node.port}"
+            ];
+          }
+        ];
+      }
+    ];
+  };
+
+  services.grafana = {
+    enable = true;
+    settings = {
+      server = {
+        http_addr = "127.0.0.1";
+        http_port = 3000;
+        enforce_domain = true;
+        enable_gzip = true;
+        domain = grafanaDomain;
+      };
+      analytics.reporting_enabled = false;
+    };
+  };
+
+  services.nginx = {
+    enable = true;
+    virtualHosts."${grafanaDomain}" = {
+      addSSL = true;
+      enableACME = true;
+      locations."/" = {
+        proxyPass = "http://${toString config.services.grafana.settings.server.http_addr}:${toString config.services.grafana.settings.server.http_port}";
+        proxyWebsockets = true;
+        recommendedProxySettings = true;
+      };
+    };
+  };
+
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "noa@requiem.garden";
   };
 
   networking.firewall.allowedTCPPorts = [
