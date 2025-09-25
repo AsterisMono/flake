a69d68ac1bc7524b9f4b96379149ddd239d0181c
Noa Virellia <cmiki@amono.me>
2025-09-25 11:13:45 +0800

    feat(apps/feishu): ditch nixpak

--- homeModules/apps/feishu/default.nix
+++ homeModules/apps/feishu/default.nix
@@ -1,41 +1,6 @@
 { pkgs, ... }:
-let
-  feishu-nixpak = pkgs.lib.mkNixPak {
-    config =
-      { sloth, ... }:
-      {
-        imports = [
-          ../_nixpakModules/gui-base.nix
-          ../_nixpakModules/network.nix
-        ];
-        app.package = pkgs.feishu;
-        flatpak.appId = "cn.feishu.Feishu";
-        bubblewrap = {
-          sockets = {
-            x11 = false;
-            wayland = true;
-            pipewire = true;
-          };
-          bind.rw = [
-            (sloth.concat' sloth.homeDir "/.config/LarkShell")
-          ];
-          bind.ro = [
-            sloth.xdgDownloadDir
-            (sloth.concat' sloth.homeDir "Pictures/Screenshots")
-          ];
-        };
-        dbus.policies = {
-          "org.gnome.keyring" = "talk";
-          "org.freedesktop.secrets" = "talk";
-          "org.gnome.ScreenSaver" = "talk";
-          "org.gnome.Shell.Screencast" = "talk";
-          "org.freedesktop.Notifications" = "talk";
-        };
-      };
-  };
-in
 {
-  home.packages = [ feishu-nixpak.config.env ];
+  home.packages = [ pkgs.feishu ];
 
   programs.niri.settings.window-rules = [
     {
