036b3d05e53bac589b51cea0b7192087cd1ac74b
Noa Virellia <cmiki@amono.me>
2025-10-23 19:19:47 +0800

    feat(hardware/azura): add opentablet driver

--- hardwares/azura.nix
+++ hardwares/azura.nix
@@ -38,4 +38,6 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  hardware.opentabletdriver.enable = true;
 }
