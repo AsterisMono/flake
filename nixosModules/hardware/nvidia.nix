80d518fe1300b32674036b4495e525cf316c860d
Noa Virellia <cmiki@amono.me>
2025-10-24 15:59:54 +0800

    feat(nixos/nvidia): enable powerManagement to fix suspend issues

--- nixosModules/hardware/nvidia.nix
+++ nixosModules/hardware/nvidia.nix
@@ -15,6 +15,7 @@
 
   hardware.nvidia = {
     modesetting.enable = true;
+    powerManagement.enable = true;
     open = false;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
