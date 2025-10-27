577c78d3044b0613f6b70b4525666da4706d19cd
Noa Virellia <cmiki@amono.me>
2025-10-27 10:27:01 +0800

    feat(packages/vicinae-gnome-ext): update source version

--- packages/vicinae-gnome-extension.nix
+++ packages/vicinae-gnome-extension.nix
@@ -5,10 +5,10 @@
 }:
 stdenvNoCC.mkDerivation {
   pname = "vicinae-gnome-extension";
-  version = "1.5.0";
+  version = "1.5.1";
   src = fetchzip {
-    url = "https://github.com/AsterisMono/vicinae-gnome-extension/releases/download/v1.5.0-fix-schemas/vicinae@dagimg-dot.shell-extension-v1.5.0.zip";
-    hash = "sha256-VCnL+ItmXJR6ihdb7marTSEJyGOjzB0G6OpLzdAfPPM=";
+    url = "https://github.com/dagimg-dot/vicinae-gnome-extension/releases/download/v1.5.1/vicinae@dagimg-dot.shell-extension-v1.5.1.zip";
+    hash = "sha256-/JEntSdmShxzayY0Cd1PjQCWeAV/AqbpqMtFUbDI0e8=";
     stripRoot = false;
   };
 
