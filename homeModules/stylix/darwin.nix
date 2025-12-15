b65321fa05df2b84138c2ecf87a00f78cfee33af
Noa Virellia <noa@requiem.garden>
2025-12-15 13:43:55 +0800

    feat(darwin): style tweaks
    Signed-off-by: Noa Virellia <noa@requiem.garden>

--- homeModules/stylix/darwin.nix
+++ homeModules/stylix/darwin.nix
@@ -2,7 +2,7 @@
 {
   stylix = {
     enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine-dawn.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine-moon.yaml";
     opacity.terminal = 0.9;
     targets.firefox.profileNames = [ "default" ];
     targets.neovim.enable = false;
