f481b0bd6786bb53665e6a004ec366eb7b3c1497
Noa Virellia <noa@requiem.garden>
2025-12-15 12:12:27 +0800

    feat(darwin): use tun mode by default
    Signed-off-by: Noa Virellia <noa@requiem.garden>

--- darwinModules/proxy.nix
+++ darwinModules/proxy.nix
@@ -5,46 +5,30 @@
   secretsPath,
   ...
 }:
-let
-  httpProxy = "http://127.0.0.1:7890";
-  socksProxy = "socks5://127.0.0.1:7890";
-  username = config.users.users.nvirellia.name;
-in
 {
-  networking.proxy = {
-    inherit httpProxy;
-    httpsProxy = httpProxy;
-    allProxy = socksProxy;
-  };
-
   sops.secrets.mihomoConfig = {
     format = "yaml";
     sopsFile = "${secretsPath}/mihomoConfig.yaml";
     key = "";
     mode = "0400";
-    owner = username;
+    owner = "root";
   };
 
-  launchd.user.agents.mihomo = {
+  launchd.daemons.mihomo = {
     serviceConfig = {
+      Label = "mihomo";
       RunAtLoad = true;
-      KeepAlive = true;
+      LimitLoadToSessionType = "System";
+      ProgramArguments = [
+        (lib.getExe pkgs.mihomo)
+        "-d"
+        "/Library/Application Support/mihomo"
+        "-f"
+        "${config.sops.secrets.mihomoConfig.path}"
+      ];
+      WorkingDirectory = "/Library/Application Support/mihomo/";
+      StandardOutPath = "/Library/Application Support/mihomo/log.out";
+      StandardErrorPath = "/Library/Application Support/mihomo/log.err";
     };
-    serviceConfig.ProgramArguments = [
-      (lib.getExe pkgs.mihomo)
-      "-d"
-      "/Users/${username}/Library/Application Support/mihomo"
-      "-f"
-      "${config.sops.secrets.mihomoConfig.path}"
-    ];
-  };
-
-  launchd.user.agents.system-proxy-setup = {
-    serviceConfig.RunAtLoad = true;
-    script = ''
-      networksetup -setwebproxy "Wi-Fi" 127.0.0.1 7890
-      networksetup -setsecurewebproxy "Wi-Fi" 127.0.0.1 7890
-      networksetup -setsocksfirewallproxy "Wi-Fi" 127.0.0.1 7890
-    '';
   };
 }
