15cbe0496998e043252d99dbaed6094796a2f771
Noa Virellia <noa@requiem.garden>
2025-12-17 09:17:15 +0800

    feat(apps/zed): correctly set session variable for multi platform
    Signed-off-by: Noa Virellia <noa@requiem.garden>

--- homeModules/apps/zed/default.nix
+++ homeModules/apps/zed/default.nix
@@ -1,4 +1,9 @@
-{ system, unstablePkgs, ... }:
+{
+  system,
+  unstablePkgs,
+  lib,
+  ...
+}:
 let
   readJson = path: builtins.fromJSON (builtins.readFile path);
 in
@@ -21,8 +26,26 @@ in
     mutableUserKeymaps = true;
   };
 
+  # https://zed.dev/docs/environment
+  # When Zed has been launched via the macOS Dock, or a GNOME or KDE icon on Linux,
+  # or an application launcher like Alfred or Raycast, it has no surrounding shell
+  # environment from which to inherit its environment variables.
+  #
+  # In order to still have a useful environment, Zed spawns a login shell in the user's
+  # home directory and gets its environment. This environment is then set on the Zed process.
+  #
+  # That means all Zed windows and projects will inherit that home directory environment.
   home.sessionVariables = {
     ZED_PREDICT_EDITS_URL = "http://172.0.161.24:9000/predict_edits";
     EDITOR = "zed --wait -n";
   };
+
+  # Session variables are now always set through the shell. This is
+  # done automatically if the shell configuration is managed by Home
+  # Manager. If not, then you must source the
+  #   ${cfg.profileDirectory}/etc/profile.d/hm-session-vars.sh
+  # file yourself.
+  #
+  # This hints we must manage zsh with home-manager if we're on aarch64-darwin.
+  programs.zsh.enable = lib.mkDefault (system == "aarch64-darwin");
 }
