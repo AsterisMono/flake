e9d34672f976a6cf7873074a579b6d9f64b1b82c
Noa Virellia <noa@requiem.garden>
2025-12-18 10:23:32 +0800

    feat(nixos): add gpt partition pattern with swap
    Signed-off-by: Noa Virellia <noa@requiem.garden>

---
+++ nixosModules/diskLayouts/gpt-bios-compat-swap.nix
@@ -0,0 +1,36 @@
+{ lib, ... }:
+{
+  disko.devices = {
+    disk = {
+      main = {
+        device = lib.mkDefault "/dev/vda";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
+              attributes = [ 0 ]; # partition attribute
+            };
+            root = {
+              end = "-8G";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+            swap = {
+              size = "100%";
+              content = {
+                type = "swap";
+                discardPolicy = "both";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
