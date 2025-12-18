572634f3ea02500f7d7423c5372aaebedbad26f6
Noa Virellia <noa@requiem.garden>
2025-12-18 16:33:50 +0800

    feat(config/mimosa): add postgres server
    Signed-off-by: Noa Virellia <noa@requiem.garden>

--- nixosConfigurations/mimosa.nix
+++ nixosConfigurations/mimosa.nix
@@ -1,4 +1,5 @@
 {
+  lib,
   nixosModules,
   ...
 }:
@@ -13,9 +14,38 @@
     role = "server";
   };
 
-  networking.firewall.allowedTCPPorts = [
-    6443
-  ];
+  services.postgresql =
+    let
+      databases = [ "pocketid" ];
+    in
+    {
+      enable = true;
+      settings = {
+        listen_addresses = lib.mkOverride 10 "0.0.0.0";
+      };
+      ensureDatabases = databases;
+      ensureUsers = builtins.map (name: {
+        inherit name;
+        ensureDBOwnership = true;
+      }) databases;
+      authentication = lib.mkOverride 10 ''
+        #type  database  DBuser       address       auth-method
+        local  all       all                        trust
+        host   all       postgres     127.0.0.1/32  scram-sha-256
+        host   sameuser  all          10.42.0.0/16  trust
+        host   all       all          0.0.0.0/0     reject
+      '';
+    };
+
+  networking.firewall = {
+    allowedTCPPorts = [
+      6443
+    ];
+    trustedInterfaces = [
+      "cni0"
+      "flannel.1"
+    ];
+  };
 
   disko.devices.disk.main.device = "/dev/sda";
 }
