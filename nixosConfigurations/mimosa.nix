4b9a3ff2938ab252e27a9e2a4e21b1364fb73f0a
Noa Virellia <noa@requiem.garden>
2026-01-10 10:05:57 +0800

    feat(config/mimosa): remove postgres
    Signed-off-by: Noa Virellia <noa@requiem.garden>

--- nixosConfigurations/mimosa.nix
+++ nixosConfigurations/mimosa.nix
@@ -1,5 +1,4 @@
 {
-  lib,
   nixosModules,
   ...
 }:
@@ -14,29 +13,6 @@
     role = "server";
     extraFlags = [ "--disable traefik" ];
   };
-
-  services.postgresql =
-    let
-      databases = [ "pocketid" ];
-    in
-    {
-      enable = true;
-      settings = {
-        listen_addresses = lib.mkOverride 10 "0.0.0.0";
-      };
-      ensureDatabases = databases;
-      ensureUsers = builtins.map (name: {
-        inherit name;
-        ensureDBOwnership = true;
-      }) databases;
-      authentication = lib.mkOverride 10 ''
-        #type  database  DBuser       address       auth-method
-        local  all       all                        trust
-        host   all       postgres     127.0.0.1/32  scram-sha-256
-        host   sameuser  all          10.42.0.0/16  trust
-        host   all       all          0.0.0.0/0     reject
-      '';
-    };
 
   networking.firewall = {
     allowedTCPPorts = [
