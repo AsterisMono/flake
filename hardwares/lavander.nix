c16bb5ce07d123107ddfda0ce4a3cd79ab7ebe6f
Noa Virellia <noa@requiem.garden>
2026-02-28 11:24:35 +0800

    feat(lavander/network): enable systemd-networkd configuration
    Signed-off-by: Noa Virellia <noa@requiem.garden>

--- hardwares/lavander.nix
+++ hardwares/lavander.nix
@@ -18,14 +18,21 @@
   boot.extraModulePackages = [ ];
 
   networking.useDHCP = false;
-  networking.interfaces."enp6s18".ipv4.addresses = [
-    {
-      address = "154.12.191.17";
-      prefixLength = 24;
-    }
-  ];
-  networking.defaultGateway = "193.41.250.250";
-  networking.nameservers = [ "8.8.8.8" ];
+  networking.useNetworkd = true;
+
+  systemd.network.networks."10-enp6s18" = {
+    matchConfig.MACAddress = "6e:6f:b6:ee:c9:b8";
+    address = [
+      "154.12.191.17/32"
+    ];
+    routes = [
+      {
+        Gateway = "193.41.250.250";
+        GatewayOnLink = true;
+      }
+    ];
+    networkConfig.DNS = [ "8.8.8.8" ];
+  };
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
