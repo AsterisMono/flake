b58bdb47001894e90c40f66ba6578ff7d7c20f8a
Noa Virellia <noa@requiem.garden>
2026-08-05 22:59:25 +0800

    disko: add disko module and xfs configuration

---
+++ modules/diskoConfigurations/xfs-with-quota.nix
@@ -0,0 +1,39 @@
+{
+  flake.diskoConfigurations.xfs-with-quota = {
+    disko.devices = {
+      disk = {
+        main = {
+          device = "/dev/disk/by-id/some-disk-id";
+          type = "disk";
+          content = {
+            type = "gpt";
+            partitions = {
+              ESP = {
+                type = "EF00";
+                size = "1G";
+                content = {
+                  type = "filesystem";
+                  format = "vfat";
+                  mountpoint = "/boot";
+                  mountOptions = [ "umask=0077" ];
+                };
+              };
+              root = {
+                size = "100%";
+                content = {
+                  type = "filesystem";
+                  format = "xfs";
+                  mountpoint = "/";
+                  mountOptions = [
+                    "defaults"
+                    "pquota"
+                  ];
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
