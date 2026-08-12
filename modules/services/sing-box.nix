fdcf09f3686a9985b78b3b8dc5f7a0c13a24a239
Noa Virellia <noa@requiem.garden>
2026-08-12 23:38:40 +0800

    feat(services): netbird

--- modules/services/sing-box.nix
+++ modules/services/sing-box.nix
@@ -1,6 +1,12 @@
 {
   flake.modules.nixos.services-sing-box = { config, ... }: {
-    sops.secrets = { inherit (config.secrets) sing-box-config; };
+    sops.secrets.sing_box_config = {
+      format = "json";
+      sopsFile = config.constants.resources.getSecretPath "sing-box.json";
+      path = "/etc/sing-box/config.json";
+      key = "";
+      restartUnits = [ "sing-box.service" ];
+    };
     services.sing-box.enable = true;
   };
 }
