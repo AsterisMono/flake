7122b87eacc444e2cf8314aae5f492a0848dba7a
Noa Virellia <i@nvirellia.im>
2026-08-24 23:11:14 +0800

    apps: init singbox-gui module

--- modules/apps/singbox-gui.nix
+++ modules/apps/singbox-gui.nix
@@ -1,6 +1,6 @@
 { inputs, ... }: {
   flake-file.inputs.sfd-nix.url = "git+https://forge.asnk.io/sugar/sfd-nix";
-  flake.modules.nixos.singbox-gui = { lib, ... }: {
+  flake.modules.nixos.singbox-gui = { config, lib, ... }: {
     imports = [ inputs.sfd-nix.nixosModules.default ];
 
     options.security.polkit.enablePkexecWrapper = lib.mkEnableOption "dummy";
@@ -14,7 +14,29 @@
           "sfd-nix.cachix.org-1:SX5EpvFvgFZXgG94/0fX1L+lUWQ90dPq0Ieor7/rDig="
         ];
       };
-      programs.sing-box-for-desktop.enable = true;
+      sops.secrets.sing_box_config = {
+        format = "json";
+        sopsFile = config.constants.resources.getSecretPath "sing-box.json";
+        key = "";
+        mode = "0440";
+        group = "wheel";
+        restartUnits = [ "sing-box-daemon.service" ];
+      };
+      systemd.services.sing-box-daemon.after = [ "sops-nix.service" ];
+      programs.sing-box-for-desktop = {
+        enable = true;
+        settings = {
+          startAtLogin = true;
+          appearance = "dark";
+          core.disableDeprecatedWarnings = true;
+        };
+        profiles = [
+          {
+            name = "Default";
+            configurationPath = config.sops.secrets.sing_box_config.path;
+          }
+        ];
+      };
     };
   };
 }
