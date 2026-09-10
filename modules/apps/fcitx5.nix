e775f9a8cf66499ef229c802d31fdc0ac177c538
Noa Virellia <i@nvirellia.im>
2026-09-11 00:40:05 +0800

    fcitx5: declare rime input method in profile

--- modules/apps/fcitx5.nix
+++ modules/apps/fcitx5.nix
@@ -12,6 +12,16 @@ _: {
             rimeDataPkgs = [ rime-ice ];
           })
         ];
+        settings.inputMethod = {
+          GroupOrder."0" = "Default";
+          "Groups/0" = {
+            Name = "Default";
+            "Default Layout" = "us";
+            DefaultIM = "rime";
+          };
+          "Groups/0/Items/0".Name = "keyboard-us";
+          "Groups/0/Items/1".Name = "rime";
+        };
       };
     };
 
