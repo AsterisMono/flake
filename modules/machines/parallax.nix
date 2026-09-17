a0e14d428f5e6e7e50882f09edf906f2cef446b2
Noa Virellia <i@nvirellia.im>
2026-09-17 23:47:11 +0800

    parallax: expose CPU temperature sensor to waybar

--- modules/machines/parallax.nix
+++ modules/machines/parallax.nix
@@ -48,6 +48,11 @@
 
         nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
         hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+        hardware.sensors.cpuTemperature = {
+          hwmonPathAbs = "/sys/devices/pci0000:00/0000:00:18.3/hwmon";
+          inputFilename = "temp1_input";
+        };
       };
     nixosModule = {
       swapDevices = [
