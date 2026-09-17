9e2550e33503e2893da416cc994b35c3c4bc6d97
Noa Virellia <i@nvirellia.im>
2026-09-17 22:28:57 +0800

    unix-tools: migrate ssh matchBlocks to settings

--- modules/apps/unix-tools.nix
+++ modules/apps/unix-tools.nix
@@ -27,9 +27,9 @@
     programs.ssh = {
       enable = true;
       enableDefaultConfig = false;
-      matchBlocks."mira" = {
-        hostname = "10.0.0.21";
-        user = "mira";
+      settings."mira" = {
+        HostName = "10.0.0.21";
+        User = "mira";
       };
     };
 
